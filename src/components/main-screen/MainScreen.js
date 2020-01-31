e91ae17cb8aa56930dda8fac435f65a2c5d326b8
mohsinghani-external <mohsinghani-external>
2020-01-31 15:36:45 +0500

    minor changes in MainScreen

--- src/components/main-screen/MainScreen.js
+++ src/components/main-screen/MainScreen.js
@@ -4,6 +4,14 @@ import UserWindow from './../users-window/UsersWindow'
 import ChatWindow from './../chat-window/ChatWindow'
 
 export class MainScreen extends Component {
+    constructor(props) {
+        super(props)
+        let token = localStorage.getItem('token')
+        if (!token) {
+            this.props.history.push('/login')
+        }
+
+    }
     render() {
         return (
             <div className="main-wrapper">
